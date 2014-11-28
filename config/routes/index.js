0b5d3aa0109e9aff32bbdd2e4d855726ec95da84
Souleymane Gnandou <souleymane.gnandou@gmail.com>
2014-11-28 13:36:29 +0100

    Fixing some issues

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,4 +1,4 @@
-module.exports = function (app){
-	app.use('/api', require('./api'));
-	app.use('/', require('./website'));
+module.exports = function(app){
+  app.use('/api', require('./api'));
+  app.use('/', require('./website'));
 }
