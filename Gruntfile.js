d13f51376d9b10c7ea628859a1fe52a08dad9efd
Souleymane Gnandou <souleymane.gnandou@gmail.com>
2014-11-30 21:21:25 +0100

    remove dest file on root folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-concat-css');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
 
   grunt.registerTask('default',['less','concat_css:cssplugins','concat_css:allcss', 'concat:jsplugins', 'concat:alljs']);
 }
