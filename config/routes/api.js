fc8d44f9ec33807d8bc1f96a54461cf8525e957d
iskglad <iskglad1@gmail.com>
2014-12-07 21:56:24 +0100

    secure api

--- config/routes/api.js
+++ config/routes/api.js
@@ -19,11 +19,12 @@ module.exports = function(passport){
     /* @all user other post methods */
 
     ApiRouter.post('/user', controllers.api.user.create);
+    ApiRouter.get('/*', passport.authenticate(BAERER, {session: false}));
     ApiRouter.post('/user/:id', controllers.api.user.update);
 
     /* @all user get methods */
 
-    ApiRouter.get('/', passport.authenticate(BAERER, {session: false}), controllers.api.welcome.index);
+    ApiRouter.get('/', controllers.api.welcome.index);
     ApiRouter.get('/users', controllers.api.user.index);
     ApiRouter.get('/user/:user_id', controllers.api.user.read);
     ApiRouter.get('/user/:id/remove', controllers.api.user.destroy);
